b66887ae5e2f27f643fe39483b5ec88a89f72961
zastrixarundell <zastrixarundell@gmail.com>
2020-07-17 00:17:46 +0200

    Downgraded username and password fetch

--- lib/pheed_the_pi_web/controllers/page_controller.ex
+++ lib/pheed_the_pi_web/controllers/page_controller.ex
@@ -2,7 +2,10 @@ defmodule PheedThePiWeb.PageController do
   use PheedThePiWeb, :controller
 
   import Plug.BasicAuth
-  plug :basic_auth, Application.compile_env!(:pheed_the_pi, :basic_auth)
+
+  @username Application.get_env(:pheed_the_pi, :basic_auth)[:username]
+  @password Application.get_env(:pheed_the_pi, :basic_auth)[:password]
+  plug :basic_auth, username: @username, password: @password
 
   def index(conn, _params) do
     render(conn, "index.html")
