a6fb86ee7cbdd2274c6991361fdb2b64781531b8
zastrixarundell <zastrixarundell@gmail.com>
2020-07-18 20:45:11 +0200

    Added random text

--- lib/pheed_the_pi_web/controllers/page_controller.ex
+++ lib/pheed_the_pi_web/controllers/page_controller.ex
@@ -7,7 +7,17 @@ defmodule PheedThePiWeb.PageController do
   @password Application.get_env(:pheed_the_pi, :basic_auth)[:password]
   plug :basic_auth, username: @username, password: @password
 
+  @sentences [
+    "What?! This works?!",
+    "Entering campaign...",
+    "Well... this is radical!",
+    "One might say this is super-slavic.",
+    "Loading Skyrim 2, thanks Todd.",
+    "One does not simply walk in-... Ugh I quit.",
+    "Loading Twitch's retarded cousin..."
+  ]
+
   def index(conn, _params) do
-    render(conn, "index.html")
+    render(conn, "index.html", message: Enum.random(@sentences) |> IO.inspect(label: "Message"))
   end
 end
